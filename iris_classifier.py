b4315da5a5cafa8825a7293269832d4b1fc65da4
sbrocks <shaury29.baranwal@gmail.com>
2018-01-27 18:34:39 +0530

    iris

---
+++ iris_classifier.py
@@ -0,0 +1,33 @@
+# Importing the libraries
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+# Importing the dataset
+dataset=pd.read_csv('IRIS.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,8].values
+
+#Splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.15,random_state=0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 50, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(y_test,"	",y_pred)
+print(cm)
